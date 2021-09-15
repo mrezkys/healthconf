e9fefad3e5e83d43268a267039e8f48414f5aa8f
mrezkys <mrezkysulihin@gmail.com>
2021-09-15 19:57:42 +0800

    feat: create navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="dark" class="navbar p-0 bg-transparent">
+    <b-navbar-brand to="/" >
+      <img  src="~assets/icons/logo.svg" alt="">
+      <h5 class="company-name d-inline">Healthconf</h5>
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav class="mr-auto">
+        <b-nav-item class="nav-item" to="/" active>Home</b-nav-item>
+        <b-nav-item class="nav-item" to="/">Relawan</b-nav-item>
+        <b-nav-item class="nav-item" to="/">Program</b-nav-item>
+        <b-nav-item class="nav-item" to="/">Tentang Kami</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav>
+        <b-nav-item class="nav-item" to="/auth">Masuk</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<style scoped>
+  .navbar{
+    padding: 20px 0 !important;
+  }
+
+  .navbar .company-name{
+      margin: 0 15px;
+      font-family: 'Visby CF';
+      font-weight: 700;
+      font-size: 22px;
+  }
+</style>
